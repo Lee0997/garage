f9bb65a29434722305b85aae256f6be51da8aab1
Lee <leenock2012@yahoo.co.uk>
2022-03-17 16:38:04 +0000

    Added some instructions in the Runner for displaying private data through a getter.

--- src/garage/Runner.java
+++ src/garage/Runner.java
@@ -1,10 +1,19 @@
 package garage;
 
+import garage.entity.Vehicle;
+
 public class Runner {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		Vehicle motorbike = new Vehicle();
+		motorbike.setId(14);
+		motorbike.setPrice(5000);
+		motorbike.setSound("Brum");
+		motorbike.setType("Bike");
+		System.out.println(motorbike.getType());
+		
 	}
 
 }
